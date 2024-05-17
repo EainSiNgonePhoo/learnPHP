994ff1485a594fb0d6a30ef73081a13940aec1cc
EainSiNgonePhoo <sanm17164@gmail.com>
2024-05-17 21:31:19 +0630

    array.php

---
+++ array.php
@@ -0,0 +1,13 @@
+<?php
+//index array 
+$data =array(10,20,30,40,50);
+echo "<pre/>";
+var_dump($data);
+echo "<br>";
+
+$data2=["name"=>"eain",
+"age"=>"21",
+"job"=>"student"];
+var_dump($data2);
+$data2["age"]="sisi";
+var_dump($data2);
